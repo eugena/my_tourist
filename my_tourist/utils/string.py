24a4febae308687335727155891fe24750350880
eugena <eugena.mihailikova@gmail.com>
2020-12-25 11:00:30 +0500

    initial commit

--- my_tourist/utils/string.py
+++ my_tourist/utils/string.py
@@ -10,4 +10,4 @@ def get_random_string(length=8):
     :return: str
     """
     letters = string.ascii_lowercase + string.ascii_uppercase + string.digits
-    return ''.join([random.choice(letters) for i in range(length)])
+    return "".join([random.choice(letters) for i in range(length)])
