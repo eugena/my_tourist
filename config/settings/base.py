87812fad40438c340db54058c1856107dd46a7aa
eugena <eugena.mihailikova@gmail.com>
2020-12-26 10:27:35 +0500

    initial commit

---
+++ config/settings/base.py
@@ -0,0 +1,555 @@
+"""
+Base settings to build other settings files upon.
+"""
+from pathlib import Path
+
+import environ
+
+ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
+# my_tourist/
+APPS_DIR = ROOT_DIR / "my_tourist"
+env = environ.Env()
+
+READ_DOT_ENV_FILE = env.bool("DJANGO_READ_DOT_ENV_FILE", default=False)
+if READ_DOT_ENV_FILE:
+    # OS environment variables take precedence over variables from .env
+    env.read_env(str(ROOT_DIR / ".env"))
+
+# GENERAL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#debug
+DEBUG = env.bool("DJANGO_DEBUG", False)
+# Local time zone. Choices are
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# though not all of them may be available with every OS.
+# In Windows, this must be set to your system time zone.
+TIME_ZONE = "Europe/Moscow"
+# https://docs.djangoproject.com/en/dev/ref/settings/#language-code
+LANGUAGE_CODE = "ru-ru"
+# https://docs.djangoproject.com/en/dev/ref/settings/#site-id
+SITE_ID = 1
+# https://docs.djangoproject.com/en/dev/ref/settings/#use-i18n
+USE_I18N = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#use-l10n
+USE_L10N = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#use-tz
+USE_TZ = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#locale-paths
+LOCALE_PATHS = [str(ROOT_DIR / "locale")]
+
+USE_THOUSAND_SEPARATOR = True
+
+# DATABASES
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#databases
+DATABASES = {"default": env.db("DATABASE_URL")}
+DATABASES["default"]["ATOMIC_REQUESTS"] = True
+
+# URLS
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#root-urlconf
+ROOT_URLCONF = "config.urls"
+# https://docs.djangoproject.com/en/dev/ref/settings/#wsgi-application
+WSGI_APPLICATION = "config.wsgi.application"
+
+# APPS
+# ------------------------------------------------------------------------------
+DJANGO_APPS = [
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    # "django.contrib.humanize", # Handy template tags
+    "django.contrib.admin",
+    "django.forms",
+]
+THIRD_PARTY_APPS = [
+    # "crispy_forms",
+    "django_icons",
+    "bootstrap4",
+    # "allauth",
+    # "allauth.account",
+    # "allauth.socialaccount",
+    "django_ipgeobase",
+]
+
+LOCAL_APPS = [
+    "my_tourist.conf.apps.ConfConfig",
+    "my_tourist.map.apps.MapConfig",
+    "my_tourist.users.apps.UsersConfig",
+]
+# https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+# MIGRATIONS
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#migration-modules
+MIGRATION_MODULES = {"sites": "my_tourist.contrib.sites.migrations"}
+
+# AUTHENTICATION
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#authentication-backends
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+    # "allauth.account.auth_backends.AuthenticationBackend",
+]
+# https://docs.djangoproject.com/en/dev/ref/settings/#auth-user-model
+# AUTH_USER_MODEL = "users.User"
+# https://docs.djangoproject.com/en/dev/ref/settings/#login-redirect-url
+# LOGIN_REDIRECT_URL = "users:redirect"
+# https://docs.djangoproject.com/en/dev/ref/settings/#login-url
+# LOGIN_URL = "account_login"
+
+
+LOGIN_URL = "/login/"
+
+AUTH_USER_MODEL = "users.User"
+
+OAUTH_PROVIDER_URL = "https://leader-id.ru"
+OAUTH_CLIENT_ID = env.str("OAUTH_CLIENT_ID", default="")
+OAUTH_CLIENT_SECRET = env.str("OAUTH_CLIENT_SECRET", default="")
+
+
+# PASSWORDS
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#password-hashers
+PASSWORD_HASHERS = [
+    # https://docs.djangoproject.com/en/dev/topics/auth/passwords/#using-argon2-with-django
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+]
+# https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+]
+
+# MIDDLEWARE
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#middleware
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.common.BrokenLinkEmailsMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+# STATIC
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#static-root
+STATIC_ROOT = str(ROOT_DIR / "staticfiles")
+# https://docs.djangoproject.com/en/dev/ref/settings/#static-url
+STATIC_URL = "/static/"
+# https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
+STATICFILES_DIRS = [str(APPS_DIR / "static")]
+# https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
+# MEDIA
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#media-root
+MEDIA_ROOT = str(APPS_DIR / "media")
+# https://docs.djangoproject.com/en/dev/ref/settings/#media-url
+MEDIA_URL = "/media/"
+
+# TEMPLATES
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#templates
+TEMPLATES = [
+    {
+        # https://docs.djangoproject.com/en/dev/ref/settings/#std:setting-TEMPLATES-BACKEND
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        # https://docs.djangoproject.com/en/dev/ref/settings/#template-dirs
+        "DIRS": [str(APPS_DIR / "templates")],
+        "OPTIONS": {
+            # https://docs.djangoproject.com/en/dev/ref/settings/#template-loaders
+            # https://docs.djangoproject.com/en/dev/ref/templates/api/#loader-types
+            "loaders": [
+                "django.template.loaders.filesystem.Loader",
+                "django.template.loaders.app_directories.Loader",
+            ],
+            # https://docs.djangoproject.com/en/dev/ref/settings/#template-context-processors
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.template.context_processors.i18n",
+                "django.template.context_processors.media",
+                "django.template.context_processors.static",
+                "django.template.context_processors.tz",
+                "django.contrib.messages.context_processors.messages",
+                "my_tourist.utils.context_processors.settings_context",
+                "my_tourist.utils.context_processors.regions_list",
+            ],
+        },
+    }
+]
+
+# https://docs.djangoproject.com/en/dev/ref/settings/#form-renderer
+FORM_RENDERER = "django.forms.renderers.TemplatesSetting"
+
+# http://django-crispy-forms.readthedocs.io/en/latest/install.html#template-packs
+# CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+# FIXTURES
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#fixture-dirs
+FIXTURE_DIRS = (str(APPS_DIR / "fixtures"),)
+
+# SECURITY
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-httponly
+SESSION_COOKIE_HTTPONLY = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-httponly
+CSRF_COOKIE_HTTPONLY = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-browser-xss-filter
+SECURE_BROWSER_XSS_FILTER = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#x-frame-options
+X_FRAME_OPTIONS = "DENY"
+
+# EMAIL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
+EMAIL_BACKEND = env(
+    "DJANGO_EMAIL_BACKEND", default="django.core.mail.backends.smtp.EmailBackend"
+)
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-timeout
+EMAIL_TIMEOUT = 5
+
+# ADMIN
+# ------------------------------------------------------------------------------
+# Django Admin URL.
+ADMIN_URL = "admin/"
+# https://docs.djangoproject.com/en/dev/ref/settings/#admins
+ADMINS = [("""Eugena A. Mikhaylikova""", "eugena.mihailikkova@gmail.com")]
+# https://docs.djangoproject.com/en/dev/ref/settings/#managers
+MANAGERS = ADMINS
+
+#
+# # django-compressor
+# # ------------------------------------------------------------------------------
+# # https://django-compressor.readthedocs.io/en/latest/settings/#django.conf.settings.COMPRESS_ENABLED
+# COMPRESS_ENABLED = env.bool("COMPRESS_ENABLED", default=True)
+# # https://django-compressor.readthedocs.io/en/latest/settings/#django.conf.settings.COMPRESS_STORAGE
+# COMPRESS_STORAGE = "compressor.storage.GzipCompressorFileStorage"
+# # https://django-compressor.readthedocs.io/en/latest/settings/#django.conf.settings.COMPRESS_URL
+# COMPRESS_URL = STATIC_URL  # noqa F405
+# # https://django-compressor.readthedocs.io/en/latest/settings/#django.conf.settings.COMPRESS_OFFLINE
+# COMPRESS_OFFLINE = True  # Offline compression is required when using Whitenoise
+# # https://django-compressor.readthedocs.io/en/latest/settings/#django.conf.settings.COMPRESS_FILTERS
+# COMPRESS_FILTERS = {
+#     "css": [
+#         "compressor.filters.css_default.CssAbsoluteFilter",
+#         "compressor.filters.cssmin.rCSSMinFilter",
+#     ],
+#     "js": ["compressor.filters.jsmin.JSMinFilter"],
+# }
+
+# LOGGING
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#logging
+# See https://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(asctime)s %(module)s "
+            "%(process)d %(thread)d %(message)s"
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        }
+    },
+    "root": {"level": "INFO", "handlers": ["console"]},
+}
+
+
+# django-allauth
+# ------------------------------------------------------------------------------
+# ACCOUNT_ALLOW_REGISTRATION = env.bool("DJANGO_ACCOUNT_ALLOW_REGISTRATION", True)
+# # https://django-allauth.readthedocs.io/en/latest/configuration.html
+# ACCOUNT_AUTHENTICATION_METHOD = "username"
+# # https://django-allauth.readthedocs.io/en/latest/configuration.html
+# ACCOUNT_EMAIL_REQUIRED = True
+# # https://django-allauth.readthedocs.io/en/latest/configuration.html
+# ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+# # https://django-allauth.readthedocs.io/en/latest/configuration.html
+# ACCOUNT_ADAPTER = "my_tourist.users.adapters.AccountAdapter"
+# # https://django-allauth.readthedocs.io/en/latest/configuration.html
+# SOCIALACCOUNT_ADAPTER = "my_tourist.users.adapters.SocialAccountAdapter"
+
+# django-compressor
+# ------------------------------------------------------------------------------
+# https://django-compressor.readthedocs.io/en/latest/quickstart/#installation
+# INSTALLED_APPS += ["compressor"]
+# STATICFILES_FINDERS += ["compressor.finders.CompressorFinder"]
+
+
+# Application
+TOURISM_TYPES = (
+    ("travel", "Путешествия"),
+    ("excursion", "Экскурсии"),
+    ("spa", "SPA и оздоровление"),
+    ("extreme", "Активный отдых и экстрим"),
+    ("outdoor", "Развлечения и общение"),
+)
+
+TOURISM_TYPE_DEFAULT = "travel"
+
+AGE = (
+    ("18-24", "от 18 до 24 лет"),
+    ("25-29", "от 25 до 29 лет"),
+    ("30-34", "от 30 до 34 лет"),
+    ("35-44", "от 35 до 44 лет"),
+    ("45-80", "от 45 до 80 лет"),
+)
+SEX = (
+    ("1", "Женщины"),
+    ("2", "Мужчины"),
+)
+
+WORD_STAT = {
+    "sleep": 0.6 if DEBUG else 3,
+    "timeout": 500,
+    "url": "https://wordstat.yandex.ru/#!/regions?=&filter=regions&words=",
+}
+
+VK_ADS = {
+    "app_id": 7033506,  # id VK приложения, не меняется
+    "api_v": "5.100",
+}
+
+#  Код региона
+DEFAULT_GLOBAL_CODE = "01"
+
+GLOBAL_CODE = env.str("MY_TOURIST_GLOBAL_CODE", default=DEFAULT_GLOBAL_CODE)
+#
+# SOCIAL_CREDENTIALS = {
+#     # Республика Карелия
+#     '10': {
+#         'WORD_STAT': {
+#             'email': '',
+#             'password': ''
+#         },
+#         'VK_ADS': {
+#             'email': '',
+#             'password': '',
+#             'account_id': 0,  # id рекламного кабинета
+#         },
+#     },
+#     # Псковская область
+#     '60': {
+#         'WORD_STAT': {
+#             'email': '',
+#             'password': ''
+#         },
+#         'VK_ADS': {
+#             'email': '',
+#             'password': '',
+#             'account_id': 0,  # id рекламного кабинета
+#         },
+#     },
+#     # Приморский край
+#     '25': {
+#         'WORD_STAT': {
+#             'email': '',
+#             'password': ''
+#         },
+#         'VK_ADS': {
+#             'email': '',
+#             'password': '',
+#             'account_id': 0,  # id рекламного кабинета
+#         },
+#     },
+#     # Костромская область
+#     '44': {
+#         'WORD_STAT': {
+#             'email': '',
+#             'password': ''
+#         },
+#         'VK_ADS': {
+#             'email': '',
+#             'password': '',
+#             'account_id': 0,  # id рекламного кабинета
+#         },
+#     },
+#     # Владимирская область
+#     '33': {
+#         'WORD_STAT': {
+#             'email': '',
+#             'password': ''
+#         },
+#         'VK_ADS': {
+#             'email': '',
+#             'password': '',
+#             'account_id': 0,  # id рекламного кабинета
+#         },
+#     },
+#     # Нижегородская область
+#     '52': {
+#         'WORD_STAT': {
+#             'email': '',
+#             'password': ''
+#         },
+#         'VK_ADS': {
+#             'email': '',
+#             'password': '',
+#             'account_id': 0,  # id рекламного кабинета
+#         },
+#     },
+#     # Хабаровский край
+#     '27': {
+#         'WORD_STAT': {
+#             'email': '',
+#             'password': ''
+#         },
+#         'VK_ADS': {
+#             'email': '',
+#             'password': '',
+#             'account_id': 0,  # id рекламного кабинета
+#         },
+#     },
+#     # Свердловская область
+#     '66': {
+#         'WORD_STAT': {
+#             'email': '',
+#             'password': ''
+#         },
+#         'VK_ADS': {
+#             'email': '',
+#             'password': '',
+#             'account_id': 0,  # id рекламного кабинета
+#         },
+#     },
+#     # Липецкая область
+#     '48': {
+#         'WORD_STAT': {
+#             'email': '',
+#             'password': ''
+#         },
+#         'VK_ADS': {
+#             'email': '',
+#             'password': '',
+#             'account_id': 0,  # id рекламного кабинета
+#         },
+#     },
+#     # Республика Адыгея
+#     '01': {
+#         'WORD_STAT': {
+#             'email': '',
+#             'password': ''
+#         },
+#         'VK_ADS': {
+#             'email': '',
+#             'password': '',
+#             'account_id': 0,  # id рекламного кабинета
+#         },
+#     },
+#     # Забайкальский край
+#     '75': {
+#         'WORD_STAT': {
+#             'email': '',
+#             'password': ''
+#         },
+#         'VK_ADS': {
+#             'email': '',
+#             'password': '',
+#             'account_id': 0,  # id рекламного кабинета
+#         },
+#     },
+#     # Республика Бурятия
+#     '03': {
+#         'WORD_STAT': {
+#             'email': '',
+#             'password': ''
+#         },
+#         'VK_ADS': {
+#             'email': '',
+#             'password': '',
+#             'account_id': 0,  # id рекламного кабинета
+#         },
+#     },
+#     # Ростовская область
+#     '61': {
+#         'WORD_STAT': {
+#             'email': '',
+#             'password': ''
+#         },
+#         'VK_ADS': {
+#             'email': '',
+#             'password': '',
+#             'account_id': 0,  # id рекламного кабинета
+#         },
+#     },
+#     # Челябинская область
+#     '74': {
+#         'WORD_STAT': {
+#             'email': '',
+#             'password': ''
+#         },
+#         'VK_ADS': {
+#             'email': '',
+#             'password': '',
+#             'account_id': 0,  # id рекламного кабинета
+#         },
+#     },
+#     # Томская область
+#     '70': {
+#         'WORD_STAT': {
+#             'email': '',
+#             'password': ''
+#         },
+#         'VK_ADS': {
+#             'email': '',
+#             'password': '',
+#             'account_id': 0,  # id рекламного кабинета
+#         },
+#     },
+#     # Сахалинская область
+#     '65': {
+#         'WORD_STAT': {
+#             'email': '',
+#             'password': ''
+#         },
+#         'VK_ADS': {
+#             'email': '',
+#             'password': '',
+#             'account_id': 0,  # id рекламного кабинета
+#         },
+#     },
+#     # Республика Крым
+#     '91': {
+#         'WORD_STAT': {
+#             'email': '',
+#             'password': ''
+#         },
+#         'VK_ADS': {
+#             'email': '',
+#             'password': '',
+#             'account_id': 0,  # id рекламного кабинета
+#         },
+#     },
+# }
