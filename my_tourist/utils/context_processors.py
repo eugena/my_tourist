3129d502dc04f59e0442d5bc4ba718609efeeb38
eugena <eugena.mihailikova@gmail.com>
2020-12-26 10:27:59 +0500

    initial commit

---
+++ my_tourist/utils/context_processors.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+
+from my_tourist.map.models import Region
+from my_tourist.utils.region import get_global_code
+
+
+def settings_context(request):
+    """Settings available by default to the templates context."""
+    # Note: we intentionally do NOT expose the entire settings
+    # to prevent accidental leaking of sensitive information
+    return {
+        "DEBUG": settings.DEBUG,
+        "TOURISM_TYPES": settings.TOURISM_TYPES,
+        "GLOBAL_CODE": get_global_code(request),
+        "OAUTH_PROVIDER_URL": settings.OAUTH_PROVIDER_URL,
+        "OAUTH_CLIENT_ID": settings.OAUTH_CLIENT_ID,
+    }
+
+
+def regions_list(_request):
+    return {"REGIONS": Region.objects.filter(is_pub=True).order_by("title")}
---
+++ my_tourist/utils/region.py
@@ -0,0 +1,50 @@
+from functools import wraps
+
+from django.conf import settings
+from django_ipgeobase.models import IPGeoBase
+from ipware import get_client_ip
+
+from my_tourist.map.models import Region
+
+
+def get_global_code(request):
+    """
+    Returns global region code
+
+    :return: str
+    """
+    global_region = request.COOKIES.get("global_region")
+
+    ip, _ = get_client_ip(request)
+
+    if global_region is None:
+        ip, _ = get_client_ip(request)
+        geo_bases = IPGeoBase.objects.by_ip(ip)
+        if geo_bases.exists():
+            geo_base = geo_bases[0]
+            region = Region.objects.filter(region=geo_base.region).first()
+            if isinstance(region, Region):
+                global_region = region.code
+
+    if global_region is None:
+        global_region = settings.GLOBAL_CODE
+
+    return global_region
+
+
+def global_region_cookie(func):
+    """
+    Декоратор для установки cookie региона портала
+    :param func:
+    :return:
+    """
+
+    @wraps(func)
+    def inner(request=None, *args, **kwargs):
+
+        response = func(request, *args, **kwargs)
+        if "global_region" not in request.COOKIES.keys():
+            response.set_cookie("global_region", get_global_code(request))
+        return response
+
+    return inner
