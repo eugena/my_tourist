0716d0655b2237c1d321214aba23bf00eedfea93
eugena <eugena.mihailikova@gmail.com>
2021-01-13 09:21:52 +0500

    feat(dev): refactoring of get_global_code (#13)

--- my_tourist/utils/region.py
+++ my_tourist/utils/region.py
@@ -7,28 +7,33 @@ from ipware import get_client_ip
 from my_tourist.map.models import Region
 
 
+def get_geo_base_region(request):
+    """
+    Returns a region object from the GeoIP base
+
+    :return: obj
+    """
+    geo_base_rec = None
+    ip, _ = get_client_ip(request)
+    geo_bases = IPGeoBase.objects.by_ip(ip)
+    if geo_bases.exists():
+        geo_base_rec = geo_bases[0]
+    return geo_base_rec
+
+
 def get_global_code(request=None):
     """
     Returns global region code
 
     :return: str
     """
-
-    def get_geo_base_region():
-        geo_base_rec = None
-        ip, _ = get_client_ip(request)
-        geo_bases = IPGeoBase.objects.by_ip(ip)
-        if geo_bases.exists():
-            geo_base_rec = geo_bases[0]
-        return geo_base_rec
-
     global_region = None
 
     if request is not None:
         global_region = request.COOKIES.get("global_region")
 
         if global_region is None:
-            geo_base_region = get_geo_base_region()
+            geo_base_region = get_geo_base_region(request)
             if geo_base_region is not None:
                 region = Region.objects.filter(region=geo_base_region.region).first()
                 if isinstance(region, Region):
